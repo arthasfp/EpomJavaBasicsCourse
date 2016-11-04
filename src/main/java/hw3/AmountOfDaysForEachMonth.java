7c4425cc13a5120227f181e2c4de74532dfb1517
Sergii <arthasfp@mail.ru>
2016-11-04 23:48:41 +0200

    Month amount refactored

--- src/main/java/hw3/AmountOfDaysForEachMonth.java
+++ src/main/java/hw3/AmountOfDaysForEachMonth.java
@@ -7,12 +7,15 @@ import java.time.Year;
  * Created by Sergii on 04.11.2016.
  */
 public class AmountOfDaysForEachMonth {
+   public static final int MONTH_AMOUNT_IN_THE_YEAR = 12;
+
    public static void getAmountOfDaysForEachMonth(int yearEx){
 
        // 2) загальну кількість днів для кожного місяця 2017 року;
 
        Year year = Year.of(yearEx);
-       for (int i = 1; i <= 12 ; i++) {
+
+       for (int i = 1; i <= MONTH_AMOUNT_IN_THE_YEAR; i++) {
            System.out.println("Кількість днів в місяці " + Month.of(i).toString() + " = " + year.atMonth(i).lengthOfMonth());
        }
    }
