342e38e33f8db2548c231392346b2be96ab1b0d7
Sergii <arthasfp@mail.ru>
2016-11-04 23:18:06 +0200

    hw4 task  1-2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import hw3.AmountOfDaysForEachMonth;
 import hw3.AmountOfDaysOffForEachMonth;
 import hw3.CurrentFormattedDate;
 import hw3.DaysToEndOfCurrentYear;
+import hw4.TwoDimentionalIntArray;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +10,18 @@ public class Main {
        CurrentFormattedDate.getCurrentFormattedDate();
        AmountOfDaysForEachMonth.getAmountOfDaysForEachMonth(2017);
        AmountOfDaysOffForEachMonth.getAmountOfDaysOffForEachMonth(2017);
+
+        TwoDimentionalIntArray array = new TwoDimentionalIntArray();
+        array.ininitializeIntArray();
+        array.printArray();
+        System.out.println();
+        array.bubblesortIntArray();
+        array.printArray();
+        System.out.println();
+
+        TwoDimentionalIntArray array1 = new TwoDimentionalIntArray();
+        array1.ininitializeIntArray();
+        array1.sortIntArray();
+        array1.printArray();
     }
 }
---
+++ src/main/java/hw4/TwoDimentionalIntArray.java
@@ -0,0 +1,58 @@
+package hw4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by Sergii on 04.11.2016.
+ */
+
+public class TwoDimentionalIntArray {
+    private final int[][] array;
+
+    public TwoDimentionalIntArray() {
+        array = new int[5][5];
+    }
+
+    public int[][] getArray() {
+        return array;
+    }
+
+    public void ininitializeIntArray(){
+        Random random = new Random();
+        for (int i = 0; i <array.length ; i++) {
+            for (int j = 0; j <array[i].length ; j++) {
+                array[i][j] = random.nextInt(200)-100;
+            }
+        }
+    }
+
+    public void bubblesortIntArray(){
+        for (int i = 0; i <array.length ; i++) {
+            for (int j = 0; j < array[i].length-1; j++) {
+                for (int k = j + 1; k < array[i].length; k++) {
+                    if (array[i][j] > array[i][k]) {
+                        int temp = array[i][j];
+                        array[i][j] = array[i][k];
+                        array[i][k] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    public void sortIntArray(){
+        for (int i = 0; i <array.length ; i++) {
+           Arrays.sort(array[i]);
+        }
+    }
+
+    public void printArray(){
+        for (int i = 0; i <array.length ; i++) {
+            System.out.println();
+            for (int j = 0; j <array[i].length ; j++) {
+                System.out.print(array[i][j]+ " ");
+            }
+        }
+    }
+}
