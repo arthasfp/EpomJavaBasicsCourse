ee7ad97a3c61d13b9aa95090a5f31369cda406d2
Sergii <arthasfp@mail.ru>
2016-11-05 00:02:23 +0200

    TwoDimentionalStringArray class

---
+++ src/main/java/hw4/TwoDimentionalStringArray.java
@@ -0,0 +1,12 @@
+package hw4;
+
+/**
+ * Created by Sergii on 05.11.2016.
+ */
+public class TwoDimentionalStringArray {
+    private final String[][] array;
+
+    public TwoDimentionalStringArray() {
+        array = new String[3][3];
+    }
+}
