24a56821c9ec6bdeb1b9ca6e131570110c0ee22a
Sergii <arthasfp@mail.ru>
2016-11-04 23:21:56 +0200

    Refactored variables names

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,17 +11,17 @@ public class Main {
        AmountOfDaysForEachMonth.getAmountOfDaysForEachMonth(2017);
        AmountOfDaysOffForEachMonth.getAmountOfDaysOffForEachMonth(2017);
 
-        TwoDimentionalIntArray array = new TwoDimentionalIntArray();
-        array.ininitializeIntArray();
-        array.printArray();
+        TwoDimentionalIntArray arrayFirst = new TwoDimentionalIntArray();
+        arrayFirst.ininitializeIntArray();
+        arrayFirst.printArray();
         System.out.println();
-        array.bubblesortIntArray();
-        array.printArray();
+        arrayFirst.bubblesortIntArray();
+        arrayFirst.printArray();
         System.out.println();
 
-        TwoDimentionalIntArray array1 = new TwoDimentionalIntArray();
-        array1.ininitializeIntArray();
-        array1.sortIntArray();
-        array1.printArray();
+        TwoDimentionalIntArray arraySecond = new TwoDimentionalIntArray();
+        arraySecond.ininitializeIntArray();
+        arraySecond.sortIntArray();
+        arraySecond.printArray();
     }
 }
