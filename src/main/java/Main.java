f7fa02feeb2eccf54e9caaf636eeb29546708140
Sergii <arthasfp@mail.ru>
2016-11-07 15:56:46 +0200

    StringTester class created isEmpty method realized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,43 +2,50 @@ import hw3.AmountOfDaysForEachMonth;
 import hw3.AmountOfDaysOffForEachMonth;
 import hw3.CurrentFormattedDate;
 import hw3.DaysToEndOfCurrentYear;
+import hw4.StringTester;
 import hw4.TwoDimentionalIntArray;
 import hw4.TwoDimentionalStringArray;
 
 public class Main {
     public static void main(String[] args) {
-       DaysToEndOfCurrentYear.getDaysToEndOfCurrentYear();
-       CurrentFormattedDate.getCurrentFormattedDate();
-       AmountOfDaysForEachMonth.getAmountOfDaysForEachMonth(2017);
-       AmountOfDaysOffForEachMonth.getAmountOfDaysOffForEachMonth(2017);
+//       DaysToEndOfCurrentYear.getDaysToEndOfCurrentYear();
+//       CurrentFormattedDate.getCurrentFormattedDate();
+//       AmountOfDaysForEachMonth.getAmountOfDaysForEachMonth(2017);
+//       AmountOfDaysOffForEachMonth.getAmountOfDaysOffForEachMonth(2017);
+//
+//        TwoDimentionalIntArray arrayFirst = new TwoDimentionalIntArray();
+//        arrayFirst.initializeIntArray();
+//        arrayFirst.printArray();
+//        System.out.println();
+//        arrayFirst.bubblesortIntArray();
+//        arrayFirst.printArray();
+//        System.out.println();
+//
+//        TwoDimentionalIntArray arraySecond = new TwoDimentionalIntArray();
+//        arraySecond.initializeIntArray();
+//        arraySecond.sortIntArray();
+//        arraySecond.printArray();
+//        System.out.println();
+//
+//        TwoDimentionalStringArray twoDimentionalFirstStringArray = new TwoDimentionalStringArray();
+//        twoDimentionalFirstStringArray.initializeStringArray();
+//        twoDimentionalFirstStringArray.printStringArray();
+//        System.out.println();
+//        twoDimentionalFirstStringArray.bubblesortStringArray();
+//        twoDimentionalFirstStringArray.printStringArray();
+//        System.out.println();
+//        TwoDimentionalStringArray twoDimentionalSecondStringArray = new TwoDimentionalStringArray();
+//        twoDimentionalSecondStringArray.initializeStringArray();
+//        twoDimentionalSecondStringArray.printStringArray();
+//        System.out.println();
+//        twoDimentionalSecondStringArray.sortStringArray();
+//        twoDimentionalSecondStringArray.printStringArray();
+//        System.out.println();
 
-        TwoDimentionalIntArray arrayFirst = new TwoDimentionalIntArray();
-        arrayFirst.initializeIntArray();
-        arrayFirst.printArray();
-        System.out.println();
-        arrayFirst.bubblesortIntArray();
-        arrayFirst.printArray();
-        System.out.println();
-
-        TwoDimentionalIntArray arraySecond = new TwoDimentionalIntArray();
-        arraySecond.initializeIntArray();
-        arraySecond.sortIntArray();
-        arraySecond.printArray();
-        System.out.println();
-
-        TwoDimentionalStringArray twoDimentionalFirstStringArray = new TwoDimentionalStringArray();
-        twoDimentionalFirstStringArray.initializeStringArray();
-        twoDimentionalFirstStringArray.printStringArray();
-        System.out.println();
-        twoDimentionalFirstStringArray.bubblesortStringArray();
-        twoDimentionalFirstStringArray.printStringArray();
-        System.out.println();
-        TwoDimentionalStringArray twoDimentionalSecondStringArray = new TwoDimentionalStringArray();
-        twoDimentionalSecondStringArray.initializeStringArray();
-        twoDimentionalSecondStringArray.printStringArray();
-        System.out.println();
-        twoDimentionalSecondStringArray.sortStringArray();
-        twoDimentionalSecondStringArray.printStringArray();
+        String test = null;
+        String test2 = "Це не пустий рядок";
+        System.out.println(StringTester.isEmpty(test));
+        System.out.println(StringTester.isEmpty(test2));
 
     }
 }
---
+++ src/main/java/hw4/StringTester.java
@@ -0,0 +1,14 @@
+package hw4;
+
+/**
+ * Created by Sergii on 07.11.2016.
+ */
+public class StringTester {
+
+    public static boolean isEmpty(String str){
+        if(str == null)
+            return true;
+        return false;
+    }
+
+}
