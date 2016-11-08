7fac446853f8aaa7e973930d5f4c24670ce56381
Sergii <arthasfp@mail.ru>
2016-11-08 11:24:21 +0200

    Gender enum was added

---
+++ src/main/java/hw5/Gender.java
@@ -0,0 +1,37 @@
+package hw5;
+
+/**
+ * Created by Sergii on 08.11.2016.
+ */
+public enum Gender {
+    MALE(1, "Чоловік") {
+        @Override
+        public void favoriteBehavior() {
+            System.out.println("Пить пиво і дивиться футбол");
+        }
+    },
+    FEMALE(2, "Жінка") {
+        @Override
+        public void favoriteBehavior() {
+            System.out.println("Вишивать шрестиком і варить борщ");
+        }
+    };
+
+    private int id;
+    private String title;
+
+    Gender(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public abstract void favoriteBehavior();
+}
